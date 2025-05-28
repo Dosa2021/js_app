8e0f5c8f84944313d405bef3dd1f5a358e506faf
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-05-28 00:10:36 -0700

    削除ボタン実装

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ const onClickAdd = () => {
     p.className = 'todo-item'
     p.innerText = inputText
 
+    const completeButon = document.createElement('button')
+    completeButon.innerText = '完了'
+    completeButon.addEventListener('click', () => {
+        console.log('完了--------------')
+    })
+
+    const deleteButon = document.createElement('button')
+    deleteButon.innerText = '削除'
+    deleteButon.addEventListener('click', () => {
+        const deleteTargt = deleteButon.closest('li')
+        document.getElementById('incomplete-list').removeChild(deleteTargt)
+    })
+
     div.appendChild(p)
+    div.appendChild(completeButon)
+    div.appendChild(deleteButon)
     li.appendChild(div)
 
     document.getElementById('incomplete-list').appendChild(li)
